a221185478783486d8f5c3fff9053ebbc271fa95
Admin <Admin@TEB123003>
2023-09-15 15:12:44 +0200

    Add project files.

---
+++ övning 2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uppgift2_1_
+{
+
+   class Program
+   {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken satd kommer du ifrån");
+            string stad = Console.ReadLine();
+            Console.WriteLine("vilken är din drömresmål?");
+            string resmål = Console.ReadLine();
+            Console.WriteLine("Hej" + stad + "sbo. jag hoppas att du får resa till" + resmål + ".");
+            Console.ReadKey();
+        }
+   }
+}
